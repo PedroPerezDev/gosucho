01a551656cb4d3cbd8f3cb42f55fa69190607bba
PedroPerezDev <perez.pedro.dev@gmail.com>
2025-06-19 20:38:07 +0200

    funcionalidad basica dark mode

--- front/script.js
+++ front/script.js
@@ -1,1 +1,6 @@
-console.log("Estoy probando que funcione");
+var btnSwith = document.getElementById("switch");
+
+btnSwith.addEventListener('click', ()=>{
+    document.body.classList.toggle("light");
+    btnSwith.classList.toggle("active");
+})
