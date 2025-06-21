f73f89407e76134945eb65f111041bfb24eafd20
PedroPerezDev <perez.pedro.dev@gmail.com>
2025-06-21 20:18:05 +0200

    borrado de la carpeta front del main

--- front/script.js
+++
@@ -1,6 +0,0 @@
-var btnSwith = document.getElementById("switch");
-
-btnSwith.addEventListener('click', ()=>{
-    document.body.classList.toggle("light");
-    btnSwith.classList.toggle("active");
-})
